ef591fc1ed24567dff4a701fbf17b77faa99c9b3
YGZ <errrrorer@foxmail.com>
2016-04-22 11:31:05 +0800

    update MergeSort

--- src/sorts/MergeSort.java
+++ src/sorts/MergeSort.java
@@ -1,5 +1,7 @@
 package sorts;
 
+import java.util.Arrays;
+
 /**
  * Created by YGZ on 2016/4/22.
  * My email : errrrorer@foxmail.com
@@ -22,9 +24,9 @@ public class MergeSort {
         //将较小的移到新的数组中，直到有一个数组遍历完；
         while (i<=mid&&j<=high){
             if (arr[i]<=arr[j]){
-                tem[k++] = arr[i];
+                tem[k++] = arr[i++];
             }else {
-                tem[k++] = arr[j];
+                tem[k++] = arr[j++];
             }
         }
         //没有遍历过的数组，将数组剩下的元素移到新数组
@@ -38,5 +40,11 @@ public class MergeSort {
         for (int k2 = 0;k2<tem.length;k2++){
             arr[k2+low] = tem[k2];
         }
+    }
+    public static void main(String[] args){
+        int[] nums = { 2, 7, 8, 3, 1, 6, 9, 0, 5, 4 };
+        MergeSort mergeSort = new MergeSort();
+        mergeSort.sort(nums,0,nums.length-1);
+        System.out.println(Arrays.toString(nums));
     }
 }
